9f7dbd9f361b55760c304cd8d73ded97fb509588
joshborseth <joshuaborseth@gmail.com>
2022-01-01 17:42:28 -0800

    tweaked finished project

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.getElementById("container");
-colors = ["red", "green", "yellow", "blue"];
+colors = ["red", "green", "yellow", "blue", "pink", "purple"];
 
 function addColor() {
-  for (let i = 0; i < 990; i++) {
+  for (let i = 0; i < 247; i++) {
     let randColor = Math.floor(Math.random() * colors.length);
     //creating squares
     let square = document.createElement("div");
