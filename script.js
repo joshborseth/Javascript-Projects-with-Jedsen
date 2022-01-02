eab2b776e6b8e46931d44ab39080dac0f062e6cc
joshborseth <joshuaborseth@gmail.com>
2022-01-01 17:29:50 -0800

    project is finished, this is my second commit which means I have to get in the habit of doing this more.

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById("container");
+colors = ["red", "green", "yellow", "blue"];
+
+function addColor() {
+  for (let i = 0; i < 990; i++) {
+    let randColor = Math.floor(Math.random() * colors.length);
+    //creating squares
+    let square = document.createElement("div");
+    container.appendChild(square);
+    square.classList.add("square");
+
+    //adding square colors
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = colors[randColor];
+    });
+
+    //removing square colors
+    square.addEventListener("mouseout", () => {
+      square.style.backgroundColor = "#333";
+    });
+  }
+}
+
+addColor();
